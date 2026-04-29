1231b94a793ec797082675ef1d8dc02540032b13
rohitlavate97 <rohitlavate97@gmail.com>
2026-04-29 11:31:42 +0530

    Adding appium framework

---
+++ AppiumFrameworkDesign/src/test/java/com/alchemist/BaseTest.java
@@ -0,0 +1,5 @@
+package com.alchemist;
+
+public class BaseTest {
+
+}
---
+++ appium-framework/src/test/java/com/alchemist/browser/BrowserBaseTest.java
@@ -0,0 +1,39 @@
+package com.alchemist.browser;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class BrowserBaseTest {
+	public AndroidDriver driver;
+	public AppiumDriverLocalService service;
+	
+	@BeforeClass
+	public void configureAppium() throws MalformedURLException {
+		service = new AppiumServiceBuilder().withIPAddress("127.0.0.1").usingPort(4723).build();
+
+		service.start();
+
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setChromedriverExecutable("//users//rahulshetty//documents//chromedriver");
+		options.setDeviceName("RohitPhone");
+		options.setApp("//users//rohit//Appium//src//test//java//resources//ApiDemos-debug.apk"); // Set app location
+																									// here
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+		service.stop();
+	}
+}
