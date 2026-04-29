98c85c168d338a77d7b545bf04ba85e87914b3d7
rohitlavate97 <rohitlavate97@gmail.com>
2026-04-29 20:49:18 +0530

    Added AndroidActions class

--- AppiumFrameworkDesign/src/main/java/com/alchemist/pageObjects/FormPage.java
+++ AppiumFrameworkDesign/src/main/java/com/alchemist/pageObjects/FormPage.java
@@ -3,14 +3,17 @@ package com.alchemist.pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import com.alchemist.utils.AndroidActions;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class FormPage {
+public class FormPage extends AndroidActions{
 	AndroidDriver driver;
 	
 	public FormPage(AndroidDriver driver) {
+		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
---
+++ AppiumFrameworkDesign/src/main/java/com/alchemist/utils/AndroidActions.java
@@ -0,0 +1,43 @@
+package com.alchemist.utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+
+public class AndroidActions {
+	AndroidDriver driver;
+
+	public AndroidActions(AndroidDriver driver) {
+		this.driver = driver;
+	}
+
+	public void longPressAction(WebElement ele) {
+		((JavascriptExecutor) driver).executeScript("mobile: longClickGesture",
+				ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(), "duration", 2000));
+	}
+
+	public void scrollToText(String text) {
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true))"
+				+ ".scrollIntoView(new UiSelector().text(\"" + text + "\"))"));
+	}
+
+	public void scrollToEndAction() {
+
+		boolean canScrollMore;
+
+		do {
+			canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap
+					.of("left", 100, "top", 100, "width", 800, "height", 1200, "direction", "down", "percent", 0.75));
+		} while (canScrollMore);
+	}
+
+	public void swipeAction(WebElement ele, String direction) {
+		((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of("elementId",
+				((RemoteWebElement) ele).getId(), "direction", direction, "percent", 0.75));
+	}
+}
